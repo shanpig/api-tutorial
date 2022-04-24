52809e1d79c344aaadd028ab8b9761c65f0f1880
shanpig <shanpigliao@gmail.com>
2022-04-24 21:48:21 +0800

    2022/4/24 修改

--- src/App.js
+++ src/App.js
@@ -1,51 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
 import getList from './api/api';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [list, setList] = useState({});
-  const [text, setText] = useState('');
+  const [registerList, setRegisterList] = useState([]);
+  const [query, setQuery] = useState('');
+  const [queryText, setQueryText] = useState('');
+
+  useEffect(() => {
+    getList({ q: query }).then((res) => {
+      setRegisterList(res.data);
+    });
+  }, [query]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <div className="data">
         <input
-          type="number"
-          onChange={(e) => {
-            setText(e.target.value);
-          }}
+          type="text"
+          value={queryText}
+          onChange={(e) => setQueryText(e.target.value)}
         />
-        <button
-          onClick={() => {
-            getList(0, text).then((data) => {
-              console.log(data);
-              setList(data);
-            });
-          }}
-        >
-          查詢
-        </button>
-        {list.data?.map((item, index) => {
-          return (
-            <ul key={index}>
-              <li style={{ textAlign: 'left' }}>{item?.['名稱']}</li>
-              <li style={{ textAlign: 'left' }}>{item?.['營業人名稱']}</li>
-              <li style={{ textAlign: 'left' }}>{item?.['營業地址']}</li>
-              <li style={{ textAlign: 'left' }}>{item?.['登記之負責人']}</li>
-            </ul>
-          );
-        })}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={() => setQuery(queryText)}>查詢</button>
+        <table>
+          <thead>
+            <th>名稱</th>
+            <th>營業人名稱</th>
+            <th>營業地址</th>
+            <th>登記之負責人</th>
+            <th>組織別名稱</th>
+            <th>統一編號</th>
+            <th>行業代號</th>
+          </thead>
+          <tbody>
+            {registerList.map(
+              ({
+                seq,
+                名稱,
+                營業人名稱,
+                營業地址,
+                登記之負責人,
+                組織別名稱,
+                統一編號,
+                行業代號,
+              }) => {
+                return (
+                  <tr key={seq}>
+                    <td>{名稱}</td>
+                    <td>{營業人名稱}</td>
+                    <td>{營業地址}</td>
+                    <td>{登記之負責人}</td>
+                    <td>{組織別名稱}</td>
+                    <td>{統一編號}</td>
+                    <td>{行業代號}</td>
+                  </tr>
+                );
+              }
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- src/api/api.js
+++ src/api/api.js
@@ -1,39 +1,43 @@
 import $ from 'jquery';
 
-const getList = (
+const getList = ({
   offset = 0,
   limit = 10,
   sort = '',
   filters = '',
   q = '',
-  fields = ''
-) => {
+  fields = '',
+}) => {
   return new Promise((resolve) => {
-    let parameter = '';
+    const urlParams = new URLSearchParams();
 
     if (offset) {
-      parameter += `offset=${offset}&`;
+      urlParams.set('offset', offset);
     }
     if (limit) {
-      parameter += `limit=${limit}&`;
+      urlParams.set('limit', limit);
     }
     if (sort) {
-      parameter += `sort=${sort}&`;
+      urlParams.set('sort', sort);
     }
     if (filters) {
-      parameter += `filters=${filters}&`;
+      urlParams.set('filters', filters);
     }
     if (q) {
-      parameter += `q=${q}&`;
+      urlParams.set('q', q);
     }
     if (fields) {
-      parameter += `fields=${fields}`;
+      urlParams.set('fields', fields);
     }
 
     $.ajax({
-      url: `https://api.kcg.gov.tw/Api/Service/Get/7d211226-7972-431a-b822-2ed53f590d50?${parameter}`,
+      url: `https://api.kcg.gov.tw/Api/Service/Get/7d211226-7972-431a-b822-2ed53f590d50?${urlParams.toString()}`,
       dataType: 'json',
       success: function (data) {
+        console.log(
+          `https://api.kcg.gov.tw/Api/Service/Get/7d211226-7972-431a-b822-2ed53f590d50?${urlParams.toString()}`
+        );
+
         resolve(data);
       },
     });
